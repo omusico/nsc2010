b5e07e1c196e9d20a8f3ff8bf6c8f38c657ccd5a
skachkov <skachkov@guns.ru>
2011-02-02 08:43:44 +0200

    shell scripts fix

--- application/modules/pdf/controllers/FormController.php
+++ application/modules/pdf/controllers/FormController.php
@@ -47,17 +47,18 @@ class Pdf_FormController extends Zend_Controller_Action
             $filename = $PNG_TEMP_DIR.'test.png';
             //processing form input
             //remember to sanitize user input in real-life solution !!!
-            $filename = $PNG_TEMP_DIR.md5($_REQUEST['data']).'.png';
+            # время-форма-водила-ранд
+            $filename = $PNG_TEMP_DIR.md5("1234567890-10-1231-1231").'.png';
             QRcode::png("1234567890-1231-21-2324", $filename, 'Q', 4, 2);
 
 
             $QRBarcode =  $PNG_WEB_DIR.basename($filename);
 
-
+            
             # KEY  -  name of the field in PDF form
             $array = array(
                 "Logo" => "/home/vlad/nsc2010/public/images/logo-bg.jpg",
-                "App_Barcodefield_page1" => $QRBarcode); /*,
+                "App_Barcodefield_page1" => '$QRBarcode' ,
                 "FormTitle" => $arr[0]["d_First_Name"] ,
                 "App_CompanyName" => $arr[0]["d_First_Name"] ,
                 "App_CompanyHBAdress" => $arr[0]["d_First_Name"] ,
@@ -68,7 +69,7 @@ class Pdf_FormController extends Zend_Controller_Action
                 "App_DOB" => $arr[0]["d_First_Name"] ,
 
                 /* CURRENT/PREVIOUS ADDRESSES (Must Be Listed For Past 3 Years): */
-                /*"AddCurrentAddresses" => null,
+                "AddCurrentAddresses" => null,
 
                 "APP_DIWAddress" => $arr[0]["d_First_Name"] ,
                 "APP_DIWAddressCity" => $arr[0]["d_First_Name"] ,
@@ -92,13 +93,13 @@ class Pdf_FormController extends Zend_Controller_Action
                 "APP_PrevAddress2FromTo" => $arr[0]["d_First_Name"] ,
 
                 /* Do you have the legal right to work in the United States?: */
-                /*"Check Box12" => true ,
+                "Check Box12" => true ,
                 "Text130" => "Yes" ,
                 "Check Box13" => false ,
                 "Text140" => "No" ,
 
                 /* DRIVER LICENSE HISTORY (Must list any license you had for the previous 3 years in all states): */
-                /*"App_State1" =>  $arr["d_First_Name"] ,
+                "App_State1" =>  $arr["d_First_Name"] ,
                 "App_CDL1" =>  $arr["d_First_Name"] ,
                 "App_CDL1Class" =>  $arr["d_First_Name"] ,
                 "App_CDL1Exp" =>  $arr["d_First_Name"] ,
@@ -117,41 +118,41 @@ class Pdf_FormController extends Zend_Controller_Action
                 "App_SuspendedCDL" =>  $arr["d_First_Name"] ,
 
                 /* DRIVING EXPERIENCE (if none, write none): */
-                /*"App_DrivExpNone" =>  null ,
+                "App_DrivExpNone" =>  null ,
 
                 "App_STType" =>  $arr["d_First_Name"] , /* straight truck */
-                /*"App_STTypeFrom" =>  $arr["d_First_Name"] ,
+                "App_STTypeFrom" =>  $arr["d_First_Name"] ,
                 "App_STTypeTo" =>  $arr["d_First_Name"] ,
                 "App_STTypeMiles" =>  $arr["d_First_Name"] ,
 
                 "App_TSType" =>  $arr["d_First_Name"] , /* tractor and semi-trailer */
-                /*"App_TSTypeFrom" =>  $arr["d_First_Name"] ,
+                "App_TSTypeFrom" =>  $arr["d_First_Name"] ,
                 "App_TSTypeTo" =>  $arr["d_First_Name"] ,
                 "App_TSTypeMiles" =>  $arr["d_First_Name"] ,
 
                 "App_TTType" =>  $arr["d_First_Name"] , /* tractor or two trailers */
-                /*"App_TTTypeFrom" =>  $arr["d_First_Name"] ,
+                "App_TTTypeFrom" =>  $arr["d_First_Name"] ,
                 "App_TTTypeTo" =>  $arr["d_First_Name"] ,
                 "App_TTTypeMiles" =>  $arr["d_First_Name"] ,
 
                 "App_BusType" =>  $arr["d_First_Name"] , /* motor coach / school bus */
-                /*"App_BusTypeFrom" =>  $arr["d_First_Name"] ,
+                "App_BusTypeFrom" =>  $arr["d_First_Name"] ,
                 "App_BusTypeTo" =>  $arr["d_First_Name"] ,
                 "App_BusTypeMiles" =>  $arr["d_First_Name"] ,
 
                 "App_OtherType" =>  $arr["d_First_Name"] , /* other */
-                /*"App_OtherTypeFrom" =>  $arr["d_First_Name"] ,
+                "App_OtherTypeFrom" =>  $arr["d_First_Name"] ,
                 "App_OtherTypeTo" =>  $arr["d_First_Name"] ,
                 "App_OtherTypeMiles" =>  $arr["d_First_Name"] ,
                 /* end DRIVING EXPERIENCE */
 
-                /*"Text1" =>  $arr["d_First_Name"] ,
+                "Text1" =>  $arr["d_First_Name"] ,
                 /* ----->8------ 1st page ends here */
 
-                /*"App_barcodefield_page2" =>  $arr["d_First_Name"] ,
+                "App_barcodefield_page2" =>  $arr["d_First_Name"] ,
 
                 /* Employment history: */
-                /*"App_Employer_None" =>  null ,
+                "App_Employer_None" =>  null ,
 
                 "App_Employer1" =>  $arr["d_First_Name"] ,
                 "App_EmpAddress1" =>  $arr["d_First_Name"] ,
@@ -165,15 +166,15 @@ class Pdf_FormController extends Zend_Controller_Action
                 "App_EmpTo1" =>  $arr["d_First_Name"] ,
                 "App_EmpJob1" =>  $arr["d_First_Name"] ,
                 "t1" =>  $arr["d_First_Name"] , /* salary */
-                /*"App_EmpReason1" =>  $arr["d_First_Name"] ,
+                "App_EmpReason1" =>  $arr["d_First_Name"] ,
                 "Text7" => "Yes" ,
                 "Text8" => "No" ,
                 "Check Box6" => false , /* DOT_Safety_Sensitive_Function  Yes */
-                /*"Check Box61" => false ,/* DOT_Safety_Sensitive_Function  No */
-                /*"Check Box62" => false , /* FMCSR Yes */
-                /*"Check Box63" => false , /* FMCSR No */
+                "Check Box61" => false ,/* DOT_Safety_Sensitive_Function  No */
+                "Check Box62" => false , /* FMCSR Yes */
+                "Check Box63" => false , /* FMCSR No */
 
-                /*"App_Employer2" =>  $arr["d_First_Name"] ,
+                "App_Employer2" =>  $arr["d_First_Name"] ,
                 "App_EmpAddress2" =>  $arr["d_First_Name"] ,
                 "App_EmpAddress2City" =>  $arr["d_First_Name"] ,
                 "App_EmpAddress2State" =>  $arr["d_First_Name"] ,
@@ -185,15 +186,15 @@ class Pdf_FormController extends Zend_Controller_Action
                 "App_EmpTo2" =>  $arr["d_First_Name"] ,
                 "App_EmpJob2" =>  $arr["d_First_Name"] ,
                 "t2" =>  $arr["d_First_Name"] , /* salary */
-                /*"App_EmpReason2" =>  $arr["d_First_Name"] ,
+                "App_EmpReason2" =>  $arr["d_First_Name"] ,
                 "Text7" => "Yes" ,
                 "Text8" => "No" ,
                 "Check Box64" => false , /* DOT_Safety_Sensitive_Function  Yes */
-                /*"Check Box65" => false ,/* DOT_Safety_Sensitive_Function  No */
-                /*"Check Box79" => false , /* FMCSR Yes */
-                /*"Check Box7" => false , /* FMCSR No */
+                "Check Box65" => false ,/* DOT_Safety_Sensitive_Function  No */
+                "Check Box79" => false , /* FMCSR Yes */
+                "Check Box7" => false , /* FMCSR No */
 
-                /*"App_Employer3" =>  $arr["d_First_Name"] ,
+                "App_Employer3" =>  $arr["d_First_Name"] ,
                 "App_EmpAddress3" =>  $arr["d_First_Name"] ,
                 "App_EmpAddress3City" =>  $arr["d_First_Name"] ,
                 "App_EmpAddress3State" =>  $arr["d_First_Name"] ,
@@ -205,15 +206,15 @@ class Pdf_FormController extends Zend_Controller_Action
                 "App_EmpTo3" =>  $arr["d_First_Name"] ,
                 "App_EmpJob3" =>  $arr["d_First_Name"] ,
                 "t3" =>  $arr["d_First_Name"] , /* salary */
-                #"App_EmpReason3" =>  $arr["d_First_Name"] ,
-                #"Text7" => "Yes" ,
-                #"Text8" => "No" ,
-                #"Check Box68" => false , /* DOT_Safety_Sensitive_Function  Yes */
-                #"Check Box9" => false ,/* DOT_Safety_Sensitive_Function  No */
-                #"Check Box8" => false , /* FMCSR Yes */
-                #"Check Box71" => false , /* FMCSR No */
+                "App_EmpReason3" =>  $arr["d_First_Name"] ,
+                "Text7" => "Yes" ,
+                "Text8" => "No" ,
+                "Check Box68" => false , /* DOT_Safety_Sensitive_Function  Yes */
+                "Check Box9" => false ,/* DOT_Safety_Sensitive_Function  No */
+                "Check Box8" => false , /* FMCSR Yes */
+                "Check Box71" => false , /* FMCSR No */
 
-                /*"App_Employer4" =>  $arr["d_First_Name"] ,
+                "App_Employer4" =>  $arr["d_First_Name"] ,
                 "App_EmpAddress4" =>  $arr["d_First_Name"] ,
                 "App_EmpAddress4City" =>  $arr["d_First_Name"] ,
                 "App_EmpAddress4State" =>  $arr["d_First_Name"] ,
@@ -225,46 +226,46 @@ class Pdf_FormController extends Zend_Controller_Action
                 "App_EmpTo4" =>  $arr["d_First_Name"] ,
                 "App_EmpJob4" =>  $arr["d_First_Name"] ,
                 "t4" =>  $arr["d_First_Name"] , /* salary */
-                /*"App_EmpReason4" =>  $arr["d_First_Name"] ,
+                "App_EmpReason4" =>  $arr["d_First_Name"] ,
                 "Text7" => "Yes" ,
                 "Text8" => "No" ,
                 "Check Box72" => false , /* DOT_Safety_Sensitive_Function  Yes */
-                #"Check Box73" => false ,/* DOT_Safety_Sensitive_Function  No */
-                #"Check Box74" => false , /* FMCSR Yes */
-                #"Check Box75" => false , /* FMCSR No */
+                "Check Box73" => false ,/* DOT_Safety_Sensitive_Function  No */
+                "Check Box74" => false , /* FMCSR Yes */
+                "Check Box75" => false , /* FMCSR No */
 
-                #"Footer" =>  $arr["d_First_Name"] ,
+                "Footer" =>  $arr["d_First_Name"] ,
                 /* ----->8------ 2nd page ends here */
-                #"App_barcodefield_page3" =>  $arr["d_First_Name"] ,
+                "App_barcodefield_page3" =>  $arr["d_First_Name"] ,
 
                 /* ACCIDENT RECORDS for past 3 years (attache sheet if nessesary, if none, write NONE): */
-                #"App_AccNone" =>  null,
+                "App_AccNone" =>  null,
 
                 /* x6 */
-                /*"App_AccDate1" =>  $arr["d_First_Name"] ,
+                "App_AccDate1" =>  $arr["d_First_Name"] ,
                 "App_AccType1" =>  $arr["d_First_Name"] ,
                 "App_Fatalies1" =>  $arr["d_First_Name"] ,
                 "App_Injuries1" =>  $arr["d_First_Name"] ,
                 /* x6 */
 
                 /* TRAFFIC CONVICTIONS and forfeitures for past 3 years (if none, write NONE): */ /* VIOLITATIONS */
-                #"App_ViolationNone" =>  null,
+                "App_ViolationNone" =>  null,
 
                 /* x6 */
-                /*"App_Location1" =>  $arr["d_First_Name"] ,
+                "App_Location1" =>  $arr["d_First_Name"] ,
                 "App_ViolationDate1" =>  $arr["d_First_Name"] ,
                 "App_Offense1" =>  $arr["d_First_Name"] ,
                 "App_Penalty1" =>  $arr["d_First_Name"] ,
                 /* x6 */
 
 
-                #"NameCompanyOnly" =>  $arr["d_First_Name"] ,
-                #"Text4" =>  $arr["d_First_Name"] ,
-                #"Text3" =>  $arr["d_First_Name"] ,
+                "NameCompanyOnly" =>  $arr["d_First_Name"] ,
+                "Text4" =>  $arr["d_First_Name"] ,
+                "Text3" =>  $arr["d_First_Name"] ,
 
 
-                #"Footer" => $date->toString("MM/dd/yyyy")
-            #);
+                "Footer" => $date->toString("MM/dd/yyyy")
+            );
 
         }
 
@@ -272,9 +273,9 @@ class Pdf_FormController extends Zend_Controller_Action
         $one = array();
         $two = array();
         $three = array();
-
+        
         # creating temp file which contains fields for empty PDF-form:
-        $md5rand = md5(rand(1,9999));
+        $md5rand = rand(1000,9999)."_".rand(1000,9999);
         $file_name = "documents/_tmp/pdf_fdf/data_".$md5rand. "___" . date("Y-m-d").".fdf";
         @unlink("/home/vlad/nsc2010/public/$file_name");
         $fdf = forge_fdf("localhost", $array, $one, $two, $three);
@@ -289,9 +290,14 @@ class Pdf_FormController extends Zend_Controller_Action
         $cmd = "/home/vlad/nsc2010/public/shell/pdftk '/home/vlad/nsc2010/public/documents/pdf/$pdf_form' fill_form '/home/vlad/nsc2010/public/$file_name' output '/home/vlad/nsc2010/public/documents/_tmp/filled_pdf/$pfile'";
 
         shell_exec($cmd);
+        # remove temp files:
+        #@unlink("/home/vlad/nsc2010/public/$file_name");
+
+        # show document to user:
         header("Content-type: application/pdf");
         readfile("/home/vlad/nsc2010/public/documents/_tmp/filled_pdf/$pfile");
-        
+        #@unlink("/home/vlad/nsc2010/public/documents/_tmp/filled_pdf/$pfile");
+
     }
 
 }
